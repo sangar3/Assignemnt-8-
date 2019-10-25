91bc4587c79fc4b6f3591c1baed4d0da9f304551
sangar3 <55110746+sangar3@users.noreply.github.com>
2019-10-25 12:12:54 -0500

    singleton part

---
+++ Assignemnt-8/Assets/Assignment8_Assets/Assignment8_scripts/UIManager.cs
@@ -0,0 +1,45 @@
+﻿/*
+		 * (Santiago Garcia II)
+		 * (SpawnManager.cs)
+		 * (Assignment 8)
+		 * (creates a singleton )
+	*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIManager : MonoBehaviour
+{
+    
+    public class SimpleSingleton : MonoBehaviour
+    {
+        public GameObject worldspacetext;
+
+        public static SimpleSingleton instance;
+
+        void Awake()
+        {
+         
+            instance = this;
+        }
+
+     
+        public void DisplayPrompt()
+        {
+            worldspacetext.SetActive(true);
+           
+        }
+
+        
+        void UsageExample()
+        {
+           
+            SimpleSingleton.instance.DisplayPrompt();
+           
+        }
+        
+    }
+}
